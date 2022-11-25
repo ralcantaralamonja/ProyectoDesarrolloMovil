78464319ae328e656f1dc49a65429b543d2a5bd8
Yeyson Cojal Ayme <yeysonrca@gmail.com>
2022-11-25 11:18:13 -0500

    Se avanzo la ventana de Tiendas  y se realizo el vinculo directo para cada region, revisarlo por favor

---
+++ app/src/main/java/com/example/tiendaartesanal/Tiendaselva.java
@@ -0,0 +1,13 @@
+package com.example.tiendaartesanal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tiendaselva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiendaselva);
+    }
+}
