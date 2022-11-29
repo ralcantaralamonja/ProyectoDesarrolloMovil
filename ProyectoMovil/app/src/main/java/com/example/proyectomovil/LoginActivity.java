5edad3d27056bb5adbb82f3b96019b09ea9de4e1
Ricardo Alcantara Lamonja <alcantararicardo04@gmail.com>
2022-11-29 01:16:45 -0500

    Corrigiendo boton registrarce redirecionando, 2 eventos click en LoginActivity

--- ProyectoMovil/app/src/main/java/com/example/proyectomovil/LoginActivity.java
+++ ProyectoMovil/app/src/main/java/com/example/proyectomovil/LoginActivity.java
@@ -2,45 +2,52 @@ package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText usuario, clave;
-    Button acceder, registrarse;
-    String user, password;
-    @SuppressLint("MissingInflatedId")
+public class LoginActivity extends AppCompatActivity   {
+    String user = "", password =""; //
+    EditText usuario,clave;
+    Button registrarse,acceder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        usuario = (EditText) findViewById(R.id.editexusuario);
-        clave = (EditText) findViewById(R.id.editexclave);
-        registrarse = (Button) findViewById(R.id.btnRegistrar);
-        acceder = (Button) findViewById(R.id.btnIngresar);
-        acceder.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        user = usuario.getText().toString();
-        password = clave.getText().toString();
-        if (user.equals("user") && password.equals("clave")) {
-            Intent siguiente = new Intent(this,Welcome.class);
-            startActivity(siguiente);
-        } else {
-            Intent siguiente = new Intent(this,Warning.class);
-            startActivity(siguiente);
-        }
+        //Mandamos a buscar el valor por referencia por id
+        usuario  = findViewById(R.id.editexusuario);
+        clave = findViewById(R.id.editexclave);
+        registrarse =  findViewById(R.id.btnRegistrar);
+        acceder =  findViewById(R.id.btnIngresar);
+        //Creamos evento click para boton acceder
+        acceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //obtenemos los valores digitados por el usuario
+                user = usuario.getText().toString();
+                password = clave.getText().toString();
+                //generamos validacion
+                if (user.equals("user") && password.equals("clave")) {
+                    Intent siguiente = new Intent(LoginActivity.this,Welcome.class);
+                    startActivity(siguiente);
+                } else {
+                    Intent siguiente = new Intent(LoginActivity.this,Warning.class);
+                    startActivity(siguiente);
+                }
+            }
+        });
+        //Creamos evento click para boton registrarse
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(view.getContext(),UserRegister.class));
+                Intent siguiente = new Intent(LoginActivity.this,UserRegister.class);
+                startActivity(siguiente);
             }
         });
     }
+
 }
+
--- ProyectoMovil/app/src/main/java/com/example/proyectomovil/MainActivity.java
+++ ProyectoMovil/app/src/main/java/com/example/proyectomovil/MainActivity.java
@@ -16,23 +16,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //indicamos que utilice toda la pantalla
         getWindow().setFlags (WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        //indicamos las animaciones que se utilizaran
         Animation animacion1 = AnimationUtils. loadAnimation(  this,R.anim.arriba);
         Animation animacion2  = AnimationUtils. loadAnimation( this,R.anim.abajo);
+        //Mandamos a buscar el valor por referencia por id
         TextView text1 = findViewById(R.id.txtvLoading1);
         TextView text2 = findViewById(R.id.txtvLoading2);
         ImageView img1 = findViewById(R.id.imglogo);
+        //Asignamos animación aca cada objeto
         img1.setAnimation(animacion1);
         text1.setAnimation(animacion2);
         text2.setAnimation(animacion2);
+        //Creamos new HAnder para que la animación dure un tiempo determinado
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                //le indicamos a que pantalla debe pasar luego
                 Intent intent = new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },4000);
+        },4000); // indicamos el tiempo
     }
 }
